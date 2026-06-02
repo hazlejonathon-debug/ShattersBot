930ba7ccdb9f7a924c4ded952d01ed92ad994f80
hazlejonathon-debug <hazlejonathon@gmail.com>
2026-06-02 16:49:27 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -204,7 +204,7 @@ export const botConfig = {
     defaultCategory: null,
 
     // Role IDs allowed to manage/support tickets.
-    supportRoles: [],
+    supportRoles: [STAFF],
 
     // Priority options users/staff can assign.
     priorities: {
